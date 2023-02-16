fc5a6ae9251a52d494546f208faaa7c6edda879e
jatharsantosh <110289778+jatharsantosh@users.noreply.github.com>
2023-02-16 18:45:18 +0530

    Add files via upload

---
+++ studentmanagement/src/main/java/org/gl/studentmanagement/entity/User.java
@@ -0,0 +1,38 @@
+package org.gl.studentmanagement.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+import org.gl.studentmanagement.entity.*;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="user")
+public class User {
+
+	@Id
+	@Column(name="user_id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+@Column(name="username")
+	private String userName;
+@Column
+private String password;
+
+@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+@JoinTable(name = "users_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+        )
+private List<Role> roles = new ArrayList<>();
+
+
+
+
+}
+
---
+++ studentmanagement/src/main/java/org/gl/studentmanagement/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package org.gl.studentmanagement.service;
+
+import org.gl.studentmanagement.entity.User;
+import org.gl.studentmanagement.repository.UserRepository;
+import org.gl.studentmanagement.security.MyUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		User user = UserRepository.getUserByUsername(username);
+		
+		if (user == null) {
+		throw new UsernameNotFoundException(" Could not find User")	
+		}
+		
+		return new MyUserDetails(user);
+	}
+	
+}
